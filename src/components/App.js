c4c3946cf147d837e2f814b5361b116fa3e1d60f
Yuka Toshima <yka723158@gmail.com>
2022-08-30 10:13:35 +1000

    class monday

--- src/components/App.js
+++ src/components/App.js
@@ -5,8 +5,12 @@ import Reviews from "./Reviews";
 import Secrets from "./Secrets";
 import Header from "./Header";
 import Footer from "./Footer";
+import {React, useState, useEffect} from "react";
+import Quiz from "./Quiz";
 
-function App() {
+
+
+function MainPage() {
   return (
     <div className="App">
       {/* <SimpleHome/> */}
@@ -15,11 +19,72 @@ function App() {
       <Location />
       <Reviews />
       <Secrets />
-
-
-      <Footer title={"Visit again."} description={"Whatever your problem may be, my fortune-telling shall lead to the solution."} />
+      <Quiz/>
+      <Footer
+        title={"Visit again."}
+        description={
+          "Whatever your problem may be, my fortune-telling shall lead to the solution."
+        }
+      />
     </div>
   );
+}
+
+function LoadingPage() {
+  return <h1>Loading... 🚨</h1>;
+}
+
+function App() {
+  // const queryParams = new URLSearchParams(window.location.search);
+  // const isLoading = queryParams.get("loading");
+  // console.log(isLoading);
+
+  //1. use if-else condition
+  // if(isLoading === "true"){
+  //   // show loading page
+  //   return (<h1>Loading... 🚨</h1>)
+  // }else{
+  //   // show the main page
+  //   return (
+  //     <div className="App">
+  //       {/* <SimpleHome/> */}
+  //       <Header />
+  //       <About />
+  //       <Location />
+  //       <Reviews />
+  //       <Secrets />
+  //       <Footer title={"Visit again."} description={"Whatever your problem may be, my fortune-telling shall lead to the solution."} />
+  //     </div>
+  //   );
+  // }
+
+  // 2. one line
+  // return (isLoading === "true" ? <LoadingPage /> : <MainPage />);
+
+
+  // 3. Impkement a timer
+
+  // useState to create isLoading state
+  const [isLoading, setIsLoading] = useState(true)
+
+  
+  // useEffect Hook is like componentDidMount, componentDidUpdate
+  // we use it so when page is updated we can load for 2 seconds
+  useEffect(()=>{
+
+    if(isLoading){
+    // setTimeout for about 2 seconds then let isLoading to false
+    setTimeout(()=>{
+      setIsLoading(false)
+    }, 2500);
+  }
+
+
+  })
+
+  return (isLoading ? <LoadingPage /> : <MainPage />);
+
+
 }
 
 export default App;
---
+++ src/components/ReviewInput.jsx
@@ -0,0 +1,28 @@
+import { React, useState } from "react";
+
+function ReviewInput(props) {
+    const [value, setValue] = useState("");
+    const handleOnChange = (e) => {
+      // console.log("handleOnChange")
+      setValue(e.target.value);
+    };
+  
+    const handleOnClick = (e) => {
+      console.log("Clicked!")
+    }
+  
+    return (
+      <div>
+        <h3>Write me a review</h3>
+        <textarea onChange={handleOnChange} rows="4" cols="50"></textarea>
+        <p>Character count: {value.length}</p>
+        {/* <p>Word count: {value.trim().split(" ").length}</p> */}
+        <p>Word count (using regex): {(value.match(/\w+/g)||[]).length}</p>
+        <p>Value: {value}</p>
+        <button onClick={()=> props.addReviews(value)}>Add review</button>
+      </div>
+    );
+  }
+  
+  export default ReviewInput;
+  
